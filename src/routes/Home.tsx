5b508a8180415aecc3abe8765b45aa81ab62016b
ben-dev10 <calvinklien025@gmail.com>
2024-01-11 03:14:31 +0000

    chore: minor updates

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -481,12 +481,12 @@ function Services() {
             icon={<Package size={20} />}
             description="If you need any service with web development, then you've come to
               the right place: this includes coding, deployment and hosting, custom domain 
-              integrations and anything related"
+              integrations and anything related."
           />
           <ServiceTextGroup
             header="Web Design"
             icon={<Layout size={20} />}
-            description="Design new or re-design existing websites"
+            description="Design new or re-design existing websites."
           />
           <ServiceTextGroup
             header="Graphic Design"
@@ -501,7 +501,7 @@ function Services() {
                 <path d="m21.79 17.06-5.55-5.55 1.57-1.57-3.75-3.75-1.57 1.57-5.55-5.55-4.73 4.73 5.55 5.55L3 17.25V21h3.75l4.76-4.76 5.55 5.55 4.73-4.73zM9.18 11.07 5.04 6.94l1.9-1.9 1.27 1.27L7.02 7.5l1.41 1.41 1.19-1.19 1.45 1.45-1.89 1.9zm3.75 3.75 1.9-1.9 1.45 1.45-1.19 1.19 1.41 1.41 1.19-1.19 1.27 1.27-1.9 1.9-4.13-4.13zm2.203-9.697 2.538-2.539 3.748 3.748-2.538 2.539z" />
               </svg>
             }
-            description="I also offer supporting graphic design services including simple logo designs and illustration"
+            description="I also offer supporting graphic design services including simple logo designs and illustration."
           />
         </div>
       </div>
